d44ec17adaaced0f0bc29086c19540e625a6b2a9
Saurav-Goswami <32359366+Saurav-Goswami@users.noreply.github.com>
2019-08-22 17:29:49 +0530

    Update build.sh

--- case-study/build/build.sh
+++ case-study/build/build.sh
@@ -2,19 +2,19 @@
 
 # Copy the new jar to the build location
 
-cp -f /home/ubuntu//jenkins-data/pipeline/devops-master/config-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/config-service/
+cp -f /home/ubuntu/jenkins-data/pipeline/devops-master/config-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/config-service/
 
-cp -f /home/ubuntu//jenkins-data/pipeline/devops-master/discovery-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/discovery-service/
+cp -f /home/ubuntu/jenkins-data/pipeline/devops-master/discovery-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/discovery-service/
 
-cp -f /home/ubuntu//jenkins-data/pipeline/devops-master/employee-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/employee-service/
+cp -f /home/ubuntu/jenkins-data/pipeline/devops-master/employee-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/employee-service/
 
-cp -f /home/ubuntu//jenkins-data/pipeline/devops-master/organization-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/organization-service/
+cp -f /home/ubuntu/jenkins-data/pipeline/devops-master/organization-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/organization-service/
 
-cp -f /home/ubuntu//jenkins-data/pipeline/devops-master/proxy-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/proxy-service/
+cp -f /home/ubuntu/jenkins-data/pipeline/devops-master/proxy-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/proxy-service/
 
-cp -f /home/ubuntu//jenkins-data/pipeline/devops-master/gateway-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/gateway-service/
+cp -f /home/ubuntu/jenkins-data/pipeline/devops-master/gateway-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/gateway-service/
 
-cp -f /home/ubuntu//jenkins-data/pipeline/devops-master/department-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/department-service/
+cp -f /home/ubuntu/jenkins-data/pipeline/devops-master/department-service/target/*.jar /home/ubuntu/jenkins-data/pipeline/case-study/build/department-service/
 
 
 
